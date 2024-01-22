1a441483e360f2effa510fbeed22dc940da9e341
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-01-22 14:10:40 +0200

    Update Phonebook

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,45 +1,41 @@
-import { Component } from 'react';
+import { Component, useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 import styles from './phonebook.module.css';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
-import WallImage from "../wall.jpg"
+import BackImage from '../forest.jpg';
 
-class Phonebook extends Component {
-  state = {
+const Phonebook = () => {
+  const [phonebookState, setPhonebookState] = useState({
     contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     name: '',
     number: '',
     filter: '',
-  };
-  componentDidMount() {
-    const contacts = JSON.parse(localStorage.getItem("my-contacts"));
-    // if (contacts && contacts.length)
-    if (contacts?.length)  //*нова форма запису
-    
-    {this.setState({
-      contacts,
-    })}
-  }
+  });
 
-  componentDidUpdate(prevProps, prevState) {
-    
-    const { contacts } = this.state;
-    if (prevState.contacts.length !== contacts.length) {
-      localStorage.setItem('my-contacts', JSON.stringify(this.state.contacts));
-      
-    }
-    
-  }
+  useEffect(() => {
+    const storedContacts =
+      JSON.parse(localStorage.getItem('my-contacts')) || [];
+    setPhonebookState(prev => ({
+      ...prev,
+      contacts: storedContacts,
+    }));
+  }, []);
 
+  useEffect(() => {
+    localStorage.setItem(
+      'my-contacts',
+      JSON.stringify(phonebookState.contacts)
+    );
+  }, [phonebookState.contacts]);
 
-  isDublicate({ name, number }) {
-    const { contacts } = this.state;
+  const isDublicate = ({ name, number }) => {
+    const { contacts } = phonebookState;
     const normalizedName = name.toLowerCase();
     const normalizedNumber = number.toLowerCase();
 
@@ -53,88 +49,70 @@ class Phonebook extends Component {
       );
     });
     return Boolean(dublicate);
-  }
-  addContact = data => {
-    if (this.isDublicate(data)) {
+  };
+
+  const addContact = data => {
+    if (isDublicate(data)) {
       return alert(
         `This contact ${data.name}: ${data.number} is already in the book`
       );
     }
 
-    this.setState(({ contacts }) => {
-      const newContact = {
-        id: nanoid(),
-        ...data,
-      };
+    setPhonebookState(prev => {
+      const newContact = { id: nanoid(), ...data };
 
-      return { contacts: [...contacts, newContact] };
+      return { ...prev, contacts: [...prev.contacts, newContact] };
     });
   };
 
-  deleteContact = id => {
-    this.setState(({ contacts }) => {
-      const newContact = contacts.filter(item => item.id !== id);
-
-      return {
-        contacts: newContact,
-      };
-    });
+  const deleteContact = id => {
+    setPhonebookState(prev => ({
+      ...prev,
+      contacts: prev.contacts.filter(item => item.id !== id),
+    }));
   };
 
-  changeFilter = ({ target }) => {
-    this.setState({
-      filter: target.value,
-    });
+  const changeFilter = ({ target }) => {
+    setPhonebookState(prev => ({ ...prev, filter: target.value }));
   };
 
-  getFlteredContacts() {
-    const { filter, contacts } = this.state;
+  const getFilteredContacts = () => {
+    const normalizedFilter = phonebookState.filter.toLocaleLowerCase();
 
-    if (!filter) {
-      return contacts;
-    }
-    const normalizedFilter = filter.toLocaleLowerCase();
-
-    const filteredContacts = contacts.filter(({ name, number }) => {
-      const normalizedName = name.toLocaleLowerCase();
-      const normalizedNumber = number.toLocaleLowerCase();
-
+    return phonebookState.contacts.filter(({ name, number }) => {
+      const normalizedName = name.toLowerCase();
+      const normalizedNumber = number.toLowerCase();
       return (
         normalizedNumber.includes(normalizedFilter) ||
         normalizedName.includes(normalizedFilter)
       );
     });
+  };
 
-    return filteredContacts;
-  }
+  const contacts = getFilteredContacts();
 
-  render() {
-    console.log("Render!")
-    const { addContact, deleteContact, changeFilter } = this;
-    const contacts = this.getFlteredContacts();
-
-    return (
-      <div
-        className={styles.body}
-        style={{ backgroundImage: `url(${WallImage})` }}
-      >
-        <div className={styles.container}>
-          <h1 className={styles.title}>Phonebook</h1>
-          <ContactForm onSubmit={addContact} />
-          <div>
-            <h2 className={styles.title}>Contacts</h2>
-            <p className={styles.text}>Find contacts by name</p>
-            <input
-              onChange={changeFilter}
-              name="filter"
-              placeholder="Search"
-              type="text"
-            />
-            <ContactList items={contacts} deleteContact={deleteContact} />
-          </div>
+  return (
+    <div
+      className={styles.body}
+      style={{ backgroundImage: `url(${BackImage})` }}
+    >
+      <div className={styles.container}>
+        <h1 className={styles.title}>Phonebook</h1>
+        <ContactForm onSubmit={addContact} />
+        <div>
+          <h2 className={styles.title}>Contacts</h2>
+          <p className={styles.text}>Find contacts by name</p>
+          <input
+            onChange={changeFilter}
+            name="filter"
+            placeholder="Search"
+            type="text"
+          />
+          <ContactList items={contacts} deleteContact={deleteContact} />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 export default Phonebook;
