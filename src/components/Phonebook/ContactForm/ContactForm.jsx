149ca4479f8c950ddd4a83c53379ad73b2c09f1a
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-01-23 17:59:05 +0200

    Ok

--- src/components/Phonebook/ContactForm/ContactForm.jsx
+++ src/components/Phonebook/ContactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import styles from './contactForm.module.css';
 import { nanoid } from 'nanoid';
 
@@ -7,81 +7,75 @@ const INITIAL_STATE = {
   number: '',
 };
 
-class ContactForm extends Component {
-  nameId = nanoid();
-  numberId = nanoid();
+const ContactForm = ({ onSubmit }) => {
+  const [state, setState] = useState({ ...INITIAL_STATE });
 
-  state = {
-    ...INITIAL_STATE,
-  };
-
-  handleChange = ({ target }) => {
+  const handleChange = ({ target }) => {
     const { name, value } = target;
-    this.setState({
-      [name]: value,
-    });
+    setState({ ...state, [name]: value });
+    // setState(prev => ({
+    //   ...prev,
+    //   [name]: value,
+    // }));
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    this.props.onSubmit({ ...this.state });
+    onSubmit({ ...state });
     // Коли відбувається handleSubmit нам треба витягти дані з форми
-    // const { elements } = e.currentTarget;  
-    this.reset();
+    // const { elements } = e.currentTarget;
+    reset();
   };
-  reset() {
-    this.setState({ ...INITIAL_STATE });
-  }
+  const reset = () => {
+    setState({ ...INITIAL_STATE });
+  };
 
-  render() {
-    const { nameId, numberId, handleSubmit, handleChange } = this;
-    const { name, number } = this.state;
+  const nameId = nanoid();
+  const numberId = nanoid();
 
-    return (
-      <div>
-        <form onSubmit={handleSubmit} className={styles.form}>
-          <div className={styles.wrapper}>
-            <label htmlFor={nameId} className={styles.formLabel}>
-              Name
-            </label>
-            <input
-              value={name}
-              onChange={handleChange}
-              id={nameId}
-              className={styles.input}
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-             
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-              placeholder="Enter your Name."
-            />
-            <label htmlFor={numberId} className={styles.formLabel}>
-              Number
-            </label>
-            <input
-              value={number}
-              onChange={handleChange}
-              id={numberId}
-              className={styles.input}
-              type="tel"
-              name="number"
-              pattern="[0-9\+\-]*"
-              
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-              placeholder="Enter your contact"
-            />
-            <button type="submit" className={styles.btn}>
-              Add Contact
-            </button>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  const { name, number } = state;
 
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <div className={styles.wrapper}>
+          <label htmlFor={nameId} className={styles.formLabel}>
+            Name
+          </label>
+          <input
+            value={name}
+            onChange={handleChange}
+            id={nameId}
+            className={styles.input}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            placeholder="Enter your Name."
+          />
+          <label htmlFor={numberId} className={styles.formLabel}>
+            Number
+          </label>
+          <input
+            value={number}
+            onChange={handleChange}
+            id={numberId}
+            className={styles.input}
+            type="tel"
+            name="number"
+            pattern="[0-9\+\-]*"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            placeholder="Enter your contact"
+          />
+          <button type="submit" className={styles.btn}>
+            Add Contact
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default ContactForm;
